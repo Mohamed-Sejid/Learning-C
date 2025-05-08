075e2afc5f93f5c72778337e8e8a51a070b16b32
“Mohamed-Sejid” <sejidmrad@gmail.com>
2025-05-08 17:26:51 +0100

    Assignment 3

--- Assignment-03/assignment3.c
+++ Assignment-03/assignment3.c
@@ -1,17 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void)
-{
-  //initialize vars
-  char first[20];
-  char last[20];
-
-  //prompt user to input first and last name and use scanf() to store those to the initiliazed vars
+void static_mode(){
+  char name[50];
+  char last_name[50];
   printf("Enter your first name: ");
-  scanf("%s", &first);
+  scanf("%s",name);
   printf("Enter your last name: ");
-  scanf("%s", &last);
+  scanf("%s",last_name);
+  printf("Hello %s %s !",name, last_name);
 
-  //print the welcome message!
-  printf("Hello %s %s!\n", first, last);
+}
+
+void dynamic_mode(){
+  char* name;
+  char* last_name;
+  int max_char=50;
+  name = (char*)malloc(50*sizeof(char));
+  last_name = (char*)malloc(50*sizeof(char));
+  if(name==NULL || last_name==NULL){
+    fprintf(stderr,"Failed to allocate memory!!");
+    exit(1);
+  }
+  printf("Enter your first name: ");
+  fgets(name, max_char, stdin);
+  name[strcspn(name, "\n")]='\0';
+  printf("Enter your last name: ");
+  fgets(last_name, max_char, stdin);
+  last_name[strcspn(last_name, "\n")]='\0';
+  printf("Hello %s %s !",name, last_name);
+  free(name);
+  free(last_name);
+}
+int main(int argc, char** argv){
+  // static_mode();
+  dynamic_mode();
+  return 0;
 }
